64f4ddf84f6379d371e0b9197459cd4d72aef8b4
Ivica Ceraj <iceraj@gmail.com>
2013-08-27 15:04:52 -0400

    Formatter

--- src/star/genetics/genetic/impl/ModelPropertiesSheet.java
+++ src/star/genetics/genetic/impl/ModelPropertiesSheet.java
@@ -3,17 +3,18 @@ package star.genetics.genetic.impl;
 import java.io.Serializable;
 import java.util.HashMap;
 
-public class ModelPropertiesSheet implements Serializable {
+public class ModelPropertiesSheet implements Serializable
+{
 
 	private static final long serialVersionUID = 1L;
 
 	private HashMap<String, String> map = new HashMap<String, String>();
-	
-	public void put( String key, String value )
+
+	public void put(String key, String value)
 	{
 		map.put(key, value);
 	}
-	
+
 	public String get(String key)
 	{
 		return map.get(key);
