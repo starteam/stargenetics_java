f9df5a2791c2b712fdb5a32e387e8a8ae82aaa5e
Ivica Ceraj <iceraj@gmail.com>
2013-10-08 15:17:38 -0400

    Regular built files

--- src/star/genetics/Version.java
+++ src/star/genetics/Version.java
@@ -1,5 +1,5 @@
 /* Created by JReleaseInfo AntTask from Open Source Competence Group */
-/* Creation date Tue Oct 01 13:03:21 EDT 2013 */
+/* Creation date Tue Oct 08 15:13:57 EDT 2013 */
 package star.genetics;
 
 import java.util.Date;
@@ -12,11 +12,11 @@ import java.util.Date;
 public class Version {
 
 
-   /** buildDate (set during build process to 1380647001638L). */
-   private static Date buildDate = new Date(1380647001638L);
+   /** buildDate (set during build process to 1381259637579L). */
+   private static Date buildDate = new Date(1381259637579L);
 
    /**
-    * Get buildDate (set during build process to Tue Oct 01 13:03:21 EDT 2013).
+    * Get buildDate (set during build process to Tue Oct 08 15:13:57 EDT 2013).
     * @return Date buildDate
     */
    public static final Date getBuildDate() { return buildDate; }
